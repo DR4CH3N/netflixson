ca8a34ef487531cd26e67bcceefb09697331fa22
Matheus Anthony <thehellc4t@gmail.com>
2022-11-07 10:54:27 -0300

    adição de imagem e fonte

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
-import { Button, SafeAreaView, StyleSheet, Text, View } from "react-native";
-
+import {
+  Button,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { useFonts } from "expo-font";
+import logo from "./assets/images/logo.png";
 const App = () => {
+  const [fonteCarregada] = useFonts({
+    monoton: require("./assets/fonts/Monoton-Regular.ttf"),
+  });
+
+  /* a condicional abaixo serve apenas para dar um tempo suficiente para o carregamento do arquivo de fonte antes do return do componente. */
+  if (!fonteCarregada) return <Text>Fonte sendo carregada...</Text>;
+
   return (
     <SafeAreaView style={estilos.container}>
       <View style={estilos.viewLogo}>
-        <Text>Netflixson</Text>
+        <Image style={estilos.logo} source={logo} />
+        <Text style={estilos.tituloApp}>Netflixson</Text>
       </View>
 
       <View style={estilos.viewBotoes}>
@@ -24,15 +40,24 @@ export default App;
 
 const estilos = StyleSheet.create({
   container: {
-    backgroundColor: "yellow",
     flex: 1,
     alignItems: "center",
+    backgroundColor: "black",
     justifyContent: "center",
+  },
+  tituloApp: {
+    fontFamily: "monoton",
+    fontSize: 36,
+    color: "#5451a6",
+    // fontWeight: "bold",
+  },
+  logo: {
+    width: 128,
+    height: 128,
   },
   viewLogo: {
     flex: 3,
     width: "80%",
-    backgroundColor: "green",
     textAlign: "center",
     justifyContent: "flex-end",
     alignItems: "center",
@@ -41,7 +66,6 @@ const estilos = StyleSheet.create({
     flex: 2,
     justifyContent: "space-evenly",
     flexDirection: "row",
-    backgroundColor: "orange",
     width: "80%",
     alignItems: "flex-start",
   },
@@ -50,7 +74,6 @@ const estilos = StyleSheet.create({
     flex: 0.5,
     justifyContent: "space-between",
     flexDirection: "row",
-    backgroundColor: "red",
     width: "80%",
   },
 });
