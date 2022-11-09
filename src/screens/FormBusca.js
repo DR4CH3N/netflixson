d3b2404a8cffe878513f842282b9d96db5924540
Matheus Anthony <thehellc4t@gmail.com>
2022-11-09 11:50:10 -0300

    formulario com retorno de informações (alert)

--- src/screens/FormBusca.js
+++ src/screens/FormBusca.js
@@ -5,11 +5,30 @@ import {
   SafeAreaView,
   Button,
   TextInput,
+  Alert,
 } from "react-native";
 
 import { Ionicons } from "@expo/vector-icons";
+import { useState } from "react";
 
 const FormBusca = () => {
+  // captura em tempo real do que é digitado no textInput através do evento onChangeText
+  const filmeDigitado = (valorDigitado) => {
+    setFilme(valorDigitado);
+  };
+  /* Hook useState para monitorar/armazenar o filme que será buscado a partir do formulario */
+  const [filme, setFilme] = useState();
+
+  /* função chamada toda vez que o botão for pressionado (usamos a prop onPress do button) */
+  const burcarFilmes = () => {
+    /* se filme (gerenciado pelo useState) estiver vazio/undefined/false */
+    if (!filme) {
+      return Alert.alert("Ops!", "Você deve digitar um filme!");
+    }
+
+    Alert.alert("Você procurou por: ", filme);
+  };
+
   return (
     <SafeAreaView style={estilos.container}>
       <Text style={estilos.texto}>Scarface? Star wars? blade runner?</Text>
@@ -19,11 +38,20 @@ const FormBusca = () => {
 
       <View style={estilos.viewForm}>
         <Ionicons name="film" size={44} color="black" />
-        <TextInput style={estilos.campo} placeholder="pesquise aqui o filme" />
+        <TextInput
+          style={estilos.campo}
+          placeholder="pesquise aqui o filme"
+          onChangeText={filmeDigitado}
+        />
       </View>
 
       <View style={estilos.viewBotoes}>
-        <Button style={estilos.botao} color="#5451a6" title="Pesquisar filme" />
+        <Button
+          style={estilos.botao}
+          color="#5451a6"
+          title="Pesquisar filme"
+          onPress={burcarFilmes}
+        />
       </View>
     </SafeAreaView>
   );
@@ -47,6 +75,7 @@ const estilos = StyleSheet.create({
     marginVertical: 16,
   },
   campo: {
+    borderColor: "gray",
     borderWidth: 1,
     padding: 8,
     // width: "80%",
