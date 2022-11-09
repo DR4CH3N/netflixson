56b690fbb68e935b3c84f45d8797844a085f6df5
Matheus Anthony <thehellc4t@gmail.com>
2022-11-09 11:23:35 -0300

    primeira parte do exercicio feito

--- src/screens/FormBusca.js
+++ src/screens/FormBusca.js
@@ -1,14 +1,55 @@
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Button,
+  TextInput,
+} from "react-native";
+
+import { Ionicons } from "@expo/vector-icons";
 
 const FormBusca = () => {
   return (
-    <SafeAreaView>
-      <Text>FormBusca</Text>
+    <SafeAreaView style={estilos.container}>
+      <Text style={estilos.texto}>Scarface? Star wars? blade runner?</Text>
+      <Text style={estilos.texto}>
+        Localize um filme que você viu ou gostaria de ver!
+      </Text>
+
+      <View style={estilos.viewForm}>
+        <Ionicons name="film" size={44} color="black" />
+        <TextInput style={estilos.campo} placeholder="pesquise aqui o filme" />
+      </View>
+
+      <View style={estilos.viewBotoes}>
+        <Button style={estilos.botao} color="#5451a6" title="Pesquisar filme" />
+      </View>
     </SafeAreaView>
   );
 };
 
 export default FormBusca;
 
-const styles = StyleSheet.create({});
+const corPrimaria = "#5451a6";
+
+const estilos = StyleSheet.create({
+  container: {
+    padding: 16,
+    flex: 1,
+  },
+  texto: {
+    paddingVertical: 8,
+  },
+  viewForm: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 16,
+  },
+  campo: {
+    borderWidth: 1,
+    padding: 8,
+    // width: "80%",
+    flex: 0.9,
+  },
+});
