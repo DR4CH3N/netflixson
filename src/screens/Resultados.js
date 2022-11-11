51edf108e45292073fd30f8efede339fa6ed94ad
Matheus Anthony <thehellc4t@gmail.com>
2022-11-11 08:49:36 -0300

    criação do apikey (privado, adicionado anotação)

--- src/screens/Resultados.js
+++ src/screens/Resultados.js
@@ -3,18 +3,23 @@ import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import api from "./services/api";
 
+import apiKey from "../../apiKey";
+
 const Resultados = ({ route }) => {
   /* usamos a prop route (do react navigation) para acessar os parametros desta rota de navegaçao e extrair os dados (neste caso, o filme) enviados para esta tela de resultaods */
   const { filme } = route.params;
 
   const [resultados, setResultados] = useState([]);
+  /* assim que entrarmos em resultado, é executada a função async buscarFilmes que por sua vez através do axios executa */
 
   useEffect(() => {
     async function buscarFilmes() {
       try {
+        /* aguardamos a resposta da consulta GET ao endpoint "search/movie" da api. observe que este endpoint precisa de parametros para a execução correta da consulta. estes parametros DEVEM ter o mesmo nome indicado na documentação do endpoint/api */
         const resposta = await api.get("/search/movie", {
           params: {
-            api_key: "f627b1bce2605ba8ad0d24f2f55fcfc2",
+            /* api key deve ser criado aqui, por uma string ou função (não irá pro github) */
+            api_key: apiKey,
             language: "pt-br",
             query: filme,
             include_adult: false,
