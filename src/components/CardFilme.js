82a00de0e0e02099a07f31215256c78a4023e5c0
Matheus Anthony <thehellc4t@gmail.com>
2022-11-16 09:38:31 -0300

    melhor layout dos resultados

--- src/components/CardFilme.js
+++ src/components/CardFilme.js
@@ -1,17 +1,32 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, Pressable } from "react-native";
 import React from "react";
+import { Ionicons } from "@expo/vector-icons";
 
 const CardFilme = ({ filme }) => {
   const { title, poster_path } = filme;
   return (
-    <View>
+    <View style={estilos.card}>
       <Image
         style={estilos.imagem}
         source={{
           uri: `https://image.tmdb.org/t/p/original/${poster_path}`,
         }}
       />
-      <Text>{title}</Text>
+      <View style={estilos.corpo}>
+        <Text style={estilos.titulo}> {title} </Text>
+        <View style={estilos.botoes}>
+          <Pressable style={estilos.botao}>
+            <Text style={estilos.textobotao}>
+              <Ionicons name="book" size={14} color="5451a6" /> leia mais
+            </Text>
+          </Pressable>
+          <Pressable style={estilos.botao}>
+            <Text style={estilos.textobotao}>
+              <Ionicons name="add-circle" size={14} color="5451a6" /> salvar
+            </Text>
+          </Pressable>
+        </View>
+      </View>
     </View>
   );
 };
@@ -21,6 +36,41 @@ export default CardFilme;
 const estilos = StyleSheet.create({
   imagem: {
     height: 150,
-    width: 350,
+    width: 100,
+    flex: 1,
+  },
+  card: {
+    marginVertical: 4,
+    flexDirection: "row",
+    borderWidth: 2,
+    borderStyle: "solid",
+    borderColor: "black",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  botao: {
+    padding: 12,
+    borderWidth: 1,
+    borderColor: "#5451a6",
+  },
+  botoes: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginTop: 8,
+  },
+  textobotao: {
+    color: "#5451a6",
+    fontSize: 12,
+    textTransform: "uppercase",
+  },
+  titulo: {
+    backgroundColor: "#5451a6",
+    color: "white",
+    fontSize: 16,
+    paddingVertical: 8,
+    textAlign: "center",
+  },
+  corpo: {
+    flex: 2,
   },
 });
